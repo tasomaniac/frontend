500dd429b2cb17c8db3fea2d7c3a149943f58ada
Bram Kragten <mail@bramkragten.nl>
2024-12-05 13:56:28 +0100

    Add base support for sub entries

--- src/panels/config/integrations/ha-config-integrations-dashboard.ts
+++ src/panels/config/integrations/ha-config-integrations-dashboard.ts
@@ -204,6 +204,7 @@ class HaConfigIntegrationsDashboard extends SubscribeMixin(LitElement) {
           supports_remove_device: false,
           supports_unload: false,
           supports_reconfigure: false,
+          supports_subentries: false,
           pref_disable_new_entities: false,
           pref_disable_polling: false,
           disabled_by: null,
