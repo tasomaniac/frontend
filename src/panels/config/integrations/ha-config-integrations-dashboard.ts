7908c065ba660de5104c47d61925ec690306881a
Bram Kragten <mail@bramkragten.nl>
2025-01-06 14:59:39 +0100

    WIP, not functional

--- src/panels/config/integrations/ha-config-integrations-dashboard.ts
+++ src/panels/config/integrations/ha-config-integrations-dashboard.ts
@@ -204,7 +204,7 @@ class HaConfigIntegrationsDashboard extends SubscribeMixin(LitElement) {
           supports_remove_device: false,
           supports_unload: false,
           supports_reconfigure: false,
-          supported_subentries: [],
+          supported_subentry_flows: [],
           num_subentries: 0,
           pref_disable_new_entities: false,
           pref_disable_polling: false,
